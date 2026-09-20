6c381ab61d9a91a87e39ab7604e02cd116afd284
MMunim <mmunim9.9.01@gmail.com>
2026-09-20 14:35:54 +0600

    added supabase url

--- database.py
+++ database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db' # this is for sqlite database
+# SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db' # this is for sqlite database
 
 # SQLALCHEMY_DATABASE_URL='postgresql://postgres:password@localhost/ToDoApplicationDatabase' # this is for postgresql database
+
+SQLALCHEMY_DATABASE_URL='postgresql://postgres.mmklrdvabpwietcomola:munim12345munim@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres' # this is for supabase database
 
 # SQLALCHEMY_DATABASE_URL='mysql+pymysql://root:root@127.0.0.1:3306/todoapplicationdatabase' # this is for mysql database
 
